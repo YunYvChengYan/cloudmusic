c550a911823128d2de6f316cdbdbbcc60506d64c
你的哈根达斯 <2427573116@qq.com>
2021-08-22 10:23:58 +0800

    添加首页页面

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<!-- HomePage -->
+<template>
+  <div>首页</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+</style>
